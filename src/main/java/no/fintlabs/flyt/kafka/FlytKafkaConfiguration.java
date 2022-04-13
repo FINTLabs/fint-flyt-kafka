4c39afd44dc5b8e80bce4f94456e6428c083921d
Eivind Morch <eivind@morch.priv.no>
2022-04-13 19:21:30 +0200

    Add autoconfiguration

--- src/main/java/no/fintlabs/flyt/kafka/FlytKafkaConfiguration.java
+++ src/main/java/no/fintlabs/flyt/kafka/FlytKafkaConfiguration.java
@@ -1,10 +1,12 @@
 package no.fintlabs.flyt.kafka;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.ResourceUtils;
 
+@EnableAutoConfiguration
 @Configuration
 @PropertySource(ResourceUtils.CLASSPATH_URL_PREFIX + "application-flyt-kafka.properties")
 public class FlytKafkaConfiguration {
