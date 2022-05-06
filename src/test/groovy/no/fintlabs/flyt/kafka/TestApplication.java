42bbc3f9020ff1aa44a610f70ae802ac28121cb2
Eivind Morch <eivind@morch.priv.no>
2022-05-06 16:45:55 +0200

    Remove oath dependency and property configuration

--- src/test/groovy/no/fintlabs/flyt/kafka/TestApplication.java
+++ src/test/groovy/no/fintlabs/flyt/kafka/TestApplication.java
@@ -3,11 +3,9 @@ package no.fintlabs.flyt.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan("no.fintlabs")
-@Import(FlytKafkaConfiguration.class)
 public class TestApplication {
 
     public static void main(String[] args) {
