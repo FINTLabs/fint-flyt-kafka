70f2d9170f21e37383c26e4ddb2a3cedb7e35108
Eivind Morch <eivind@morch.priv.no>
2022-05-12 13:29:08 +0200

    Add support for instance flow headers in error handler

--- src/main/java/no/fintlabs/flyt/kafka/InstanceFlowErrorHandler.java
+++ src/main/java/no/fintlabs/flyt/kafka/InstanceFlowErrorHandler.java
@@ -1,10 +1,13 @@
 package no.fintlabs.flyt.kafka;
 
+import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
 import no.fintlabs.flyt.kafka.headers.InstanceFlowHeadersMapper;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.stereotype.Service;
+import org.springframework.kafka.listener.MessageListenerContainer;
 
-@Service
+
 public abstract class InstanceFlowErrorHandler extends DefaultErrorHandler {
 
     private final InstanceFlowHeadersMapper instanceFlowHeadersMapper;
@@ -12,5 +15,20 @@ public abstract class InstanceFlowErrorHandler extends DefaultErrorHandler {
     protected InstanceFlowErrorHandler(InstanceFlowHeadersMapper instanceFlowHeadersMapper) {
         this.instanceFlowHeadersMapper = instanceFlowHeadersMapper;
     }
+
+    @Override
+    public void handleRecord(Exception thrownException, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer, MessageListenerContainer container) {
+        handleInstanceFlowRecord(
+                thrownException,
+                instanceFlowHeadersMapper.getInstanceFlowHeaders(record.headers()),
+                record
+        );
+    }
+
+    public abstract void handleInstanceFlowRecord(
+            Exception thrownException,
+            InstanceFlowHeaders instanceFlowHeaders,
+            ConsumerRecord<?, ?> record
+    );
 
 }
