c625313a0e5522192235227403296c431f7612d2
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-09-02 12:52:09 +0200

    use qualified import

--- src/main/java/no/fintlabs/flyt/kafka/event/error/InstanceFlowErrorEventProducerRecord.java
+++ src/main/java/no/fintlabs/flyt/kafka/event/error/InstanceFlowErrorEventProducerRecord.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
 import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
 import no.fintlabs.kafka.model.ErrorCollection;
+import no.fintlabs.kafka.topic.name.ErrorEventTopicNameParameters;
 
 @Getter
 @EqualsAndHashCode
 @Jacksonized
 @Builder
 public class InstanceFlowErrorEventProducerRecord {
-    private no.fintlabs.kafka.topic.name.ErrorEventTopicNameParameters topicNameParameters;
+    private ErrorEventTopicNameParameters topicNameParameters;
     private InstanceFlowHeaders instanceFlowHeaders;
     private ErrorCollection errorCollection;
 }
