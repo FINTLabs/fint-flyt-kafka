8f16c64456913518da4ac053cbb598535227e9c5
Eivind Morch <eivind@morch.priv.no>
2025-12-16 11:02:55 +0100

    FFS-1613: Remove toString on records

--- src/main/java/no/novari/flyt/kafka/instanceflow/producing/InstanceFlowProducerRecord.java
+++ src/main/java/no/novari/flyt/kafka/instanceflow/producing/InstanceFlowProducerRecord.java
@@ -3,14 +3,12 @@ package no.novari.flyt.kafka.instanceflow.producing;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 import no.novari.flyt.kafka.instanceflow.headers.InstanceFlowHeaders;
 import no.novari.kafka.topic.name.TopicNameParameters;
 
 @Getter
 @Builder
 @EqualsAndHashCode
-@ToString
 public class InstanceFlowProducerRecord<V> {
     private final TopicNameParameters topicNameParameters;
     private final InstanceFlowHeaders instanceFlowHeaders;
