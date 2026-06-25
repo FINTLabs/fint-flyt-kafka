3633d823ae649967da9ba45f92168b9aef6a7bfa
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-06-25 08:26:16 +0200

    Bump version of Gradle, Spring Boot, and misc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
-    id("org.springframework.boot") version "3.5.10" apply false
+    id("org.springframework.boot") version "3.5.15" apply false
     id("java-library")
     id("maven-publish")
-    id("com.github.ben-manes.versions") version "0.53.0"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.spring") version "2.3.10"
+    id("com.github.ben-manes.versions") version "0.54.0"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    kotlin("jvm") version "2.4.0"
+    kotlin("plugin.spring") version "2.4.0"
 }
 
 group = "no.novari"
