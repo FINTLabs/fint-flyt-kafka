cb5bb70c8a7218b262d15a256e10249f145c8c44
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-20 13:09:35 +0200

    fix: overstyr sårbare avhengighetsversjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,10 @@ repositories {
     mavenLocal()
 }
 
+extra["jackson-bom.version"] = "2.21.5"
+extra["log4j2.version"] = "2.25.5"
+extra["netty.version"] = "4.1.137.Final"
+
 dependencies {
     api(platform(SpringBootPlugin.BOM_COORDINATES))
 
