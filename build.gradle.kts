1aa80115282605e111b7757d2a771becbcc9aec2
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-20 15:10:19 +0200

    fix: overstyr logback-core på ktlint-classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ repositories {
 
 dependencies {
     constraints {
+        ktlint("ch.qos.logback:logback-core:1.5.34")
         implementation("at.yawk.lz4:lz4-java:1.11.1") {
             because("Fixes CVE-2026-59949 in the kafka-clients transitive dependency")
         }
